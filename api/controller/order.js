3cc2eb4888bcf86e3393e9ce263319e61a400787
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-11 17:07:32 +0300

    order update delete  process done

--- api/controller/order.js
+++ api/controller/order.js
@@ -9,8 +9,8 @@ class OrderController {
 
   static async postOrderCont(req, res, next) {
     const orderPost = new Order({
-      shippingType: req.body.shippingType,
-      shippingAdress: req.body.shippingAdress,
+      product: req.body.product,
+      count: req.body.count,
     });
 
     orderPost
@@ -19,7 +19,36 @@ class OrderController {
         res.json(data);
       })
       .catch((error) => {
-        console.log("post işlemi ile ilgili hata var. Hata kodu : " + err);
+        console.log("post işlemi ile ilgili hata var. Hata kodu : " + error);
       });
   }
+
+  static async updateOrderCont(req, res, next) {
+    try {
+      const updatedOrder = new Order.findOne(
+        { _id: req.params.orderId },
+        { $set: { product: req.params.product } },
+        { $set: { count: req.params.count } },
+        res.json(updatedOrder)
+      );
+    } catch (error) {
+      res.json({
+        message: "Silme işlemi yapılırken hata oluştu",
+        errorCode: error,
+      });
+    }
+  }
+  static async deleteOrderCont(req, res, next) {
+    try {
+      const deletedOrder = new Order.remove({ _id: req.params.orderId });
+      res.json(deletedOrder);
+    } catch (error) {
+      res.json({
+        message: "Silme işlemi yapılırken hata oluştu",
+        errorCode: error,
+      });
+    }
+  }
 }
+
+export default OrderController;
--- api/routes/order.js
+++ api/routes/order.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import orderController from "../controller/order";
+import OrderController from "../controller/order";
 const router = Router();
 
-router.get("/", orderController);
-
-router.post("/");
+router.get("/", OrderController.getOrderCont);
+router.post("/", OrderController.postOrderCont);
+router.patch("/:orderId", OrderController.updateOrderCont);
+router.delete("/:orderId", OrderController.deleteOrderCont);
 
 export default router;
