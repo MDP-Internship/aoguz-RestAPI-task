314f2ad253c59b2a261fa0561ca815d0ed94d295
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-23 16:34:01 +0300

    add isHaveProduct logic added

--- api/utils/utils.js
+++ api/utils/utils.js
@@ -1,7 +1,8 @@
 const User = require('../model/User')
 const Product = require('../model/Product')
+var ObjectId = require('mongodb').ObjectId
 
-module.exports = async function isUser(id) {
+exports.isUser = async function isUser(id) {
   try {
     if (id.match(/^[0-9a-fA-F]{24}$/)) {
       const result = await User.findById(id)
@@ -25,5 +26,41 @@ module.exports = async function isUser(id) {
     }
   } catch (error) {
     throw error
+  }
+}
+
+exports.isHaveProduct = async function isHaveProduct(product) {
+  /*   console.log(typeof product._id)
+
+  doc = new ObjectId(product._id)
+  console.log('object ' + doc)
+  idd = product._id
+
+  var id = mongoose.ObjectId.cast(product._id)
+
+  console.log(typeof id)
+   */
+  const newArrr = product.map((item) => ObjectId(item._id))
+
+  try {
+    const result = await Product.find({
+      _id: { $in: newArrr },
+    })
+
+    if (result) {
+      console.log('sorgulanan ürün Product tablosunda mevcut')
+      return {
+        res: true,
+        product: result,
+      }
+    } else {
+      console.log('Product tablosunda ürün yok')
+      return {
+        res: false,
+        message: 'Product tablosunda ürün yok',
+      }
+    }
+  } catch (err) {
+    throw err
   }
 }
