cba5bc2094b8f9a140d712827be1c6041f1d098d
Abdullah Oğuz <oguzabdullah39@gmail.com>
2020-11-23 16:32:18 +0300

    user schema güncellendi

--- api/model/User.js
+++ api/model/User.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose')
 const orderModel = require('../model/Order.js')
 
 const userSchema = new mongoose.Schema({
-  _id: {
-    type: mongoose.Types.ObjectId,
-  },
   name: {
     type: String,
   },
   surname: {
     type: String,
   },
-  orders: [mongoose.model('Order').schema],
+  orders: [],
 })
 
 module.exports = mongoose.model('User', userSchema)
